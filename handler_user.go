6bdf65db72032a59a4a74823afd86e781d828ad5
SeanCrowe1 <sean.crowe2002@gmail.com>
2026-01-16 13:02:26 +0000

    tidied up users command function

--- handler_user.go
+++ handler_user.go
@@ -59,15 +59,14 @@ func handlerLogin(s *state, cmd command) error {
 func handlerUsers(s *state, cmd command) error {
 	users, err := s.db.GetUsers(context.Background())
 	if err != nil {
-		return fmt.Errorf("couldn't get users: %w", err)
+		return fmt.Errorf("couldn't list users: %w", err)
 	}
-
 	for _, user := range users {
 		if s.cfg.CurrentUserName == user.Name {
-			fmt.Printf("%s (current)\n", user.Name)
-		} else {
-			fmt.Printf("%s\n", user.Name)
+			fmt.Printf("* %v (current)\n", user.Name)
+			continue
 		}
+		fmt.Printf("* %v\n", user.Name)
 	}
 
 	return nil
